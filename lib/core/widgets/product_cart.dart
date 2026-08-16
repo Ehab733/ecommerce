c13fa356aafa1d155eec1f0a0923bf9061e23bcb
Ehab733 <ehabelish231@gmail.com>
2026-08-16 16:36:53 +0300

    Add internet permissions

--- lib/core/widgets/product_cart.dart
+++ lib/core/widgets/product_cart.dart
@@ -10,7 +10,6 @@ import 'package:ecommerce/features/wishlist/presentation/manager/cubit/wish_list
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:logger/logger.dart';
 
 class ProductCard extends StatelessWidget {
   final Product _product;
